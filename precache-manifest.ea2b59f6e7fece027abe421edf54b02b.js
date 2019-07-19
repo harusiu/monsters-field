d1a4559180e045b48ce13e52aaf3aed5bb834fe6
Haru Siu <siu910223@gmail.com>
2019-07-19 19:59:57 +0800

    Updates

---
+++ precache-manifest.ea2b59f6e7fece027abe421edf54b02b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c904b2d88cccc1f0ed87131d86b704",
+    "url": "/monsters-field/index.html"
+  },
+  {
+    "revision": "2cb0a426a910f4bf2a66",
+    "url": "/monsters-field/static/css/main.a73dd882.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-field/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2cb0a426a910f4bf2a66",
+    "url": "/monsters-field/static/js/main.dfa73868.chunk.js"
+  },
+  {
+    "revision": "fb5f905731349af7112f",
+    "url": "/monsters-field/static/js/runtime~main.201a89d0.js"
+  }
+]);
